59a05893b0e8c18d2f7714022ce6d4ba1284b875
Wendy Wilgus <96785281+wendywilgus@users.noreply.github.com>
2023-03-24 16:07:14 -0400

    final commit

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 let countEl = document.getElementById("count-el");
-
 let count = 0;
+let saveEl = document.getElementById("save-el");
+
 
 function increment() {
-    count = count + 1;
+    count += 1;
     countEl.innerText = count;
 }
 
 function save() {
-    console.log(count)
+    let countStr = " " + count + " - ";
+    saveEl.innerText += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
+
+
 
 increment();
 save();
+
+
+    
